b9e040db943d8921fe3302df09ef360fb7bc5356
whoiskurisu <whoiskurisu@gmail.com>
2024-03-18 14:09:27 +0545

    Adding JS files

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+// document.getElementById("logo").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
+
+
+// document.getElementById("myCart").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
+
+
+// document.getElementById("contact").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
+
+
+// document.getElementById("log-in").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
+
+
+// document.getElementById("sign-up").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
+
+document.getElementById("wlbtn").onclick = function () {
+    location.href = "wishlist.html";
+};
+
+document.getElementById("inp").onclick = function () {
+    ;
+};
